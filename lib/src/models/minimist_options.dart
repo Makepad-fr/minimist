fbb5cfbd5314abe09c01905e3c885b514a940fd0
Kaan Yagci <kaan.yagci@clique.paris>
2020-05-01 16:47:10 +0200

    [minimist-options] - number & stopEarly removed

--- lib/src/models/minimist_options.dart
+++ lib/src/models/minimist_options.dart
@@ -1,16 +1,12 @@
 class MinimistOptions {
   final List<String> string;
   final List<String> boolean;
-  final List<String> number;
   final Map<String, String> alias;
   final Map<String, String> byDefault;
-  final bool stopEarly;
 
   const MinimistOptions(
       {this.string,
       this.boolean,
-      this.number,
       this.alias,
-      this.byDefault,
-      this.stopEarly});
+      this.byDefault});
 }
